e2211a487db3b501e28db10f50c639488f533de0
propoli w <propolies@gmail.com>
2025-10-26 21:36:29 +0100

    remove constants

--- packages/components/index.ts
+++
@@ -1,4 +0,0 @@
-import { providers, type Providers } from './providers.js'
-import type { Icons } from './types.js'
-
-export { providers, type Providers, type Icons }
--- packages/components/providers.ts
+++
@@ -1,2 +0,0 @@
-export const providers = ['hero', 'lucide', 'mdi'] as const
-export type Providers = (typeof providers)[number]
